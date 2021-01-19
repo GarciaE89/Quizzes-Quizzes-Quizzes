1ad7efee09e98a522697b8f1cf262aa0558b3409
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2021-01-18 19:26:09 -0700

    initial changes

---
+++ JS/Questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        title:"Which one of the following songs is from The Weeknd's Ablbum Trilogy?",
+        options: ["Blinding Lights", "In the Night","Coming Down", "Tears in the Rain"],
+        answer: "Coming Down"
+    },
+    {
+        title:"Who is Bill's sword maker in the movie: Kill Bill Vol. 1 and Vol. 2?",
+        options: ["Miko Moto","Hattori Hanzo", "Yoshihide Suga","Katsushika Hokusai"],
+        answer: "Hattori Hanzo"
+    },
+    {
+        title:"What do you call a group of owls?",
+        options: ["Parliment", "Squad","Murder","Outlets"],
+        answer: "Parliment"
+    },
+    {
+        title:"Who is second in the line of succession for Presidency of the United States?",
+        options: ["Vice President","Secretary of State", "Speaker of the House", "Secretery of HUD"],
+        answer: "Speaker of the House"
+    }
+    {
+        title:"Sheree Whitfield is best known for what catchphrase?",
+        options: ["Get yo money up, big Dawg.", "Who gon' check me boo?","Who said that?","You're winning...tonight.."],
+        answer: "Who gon' check me boo?"
+    }
+]
